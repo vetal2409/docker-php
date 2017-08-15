5a4d3eafd55608ab09c0d1f834a5933772f5ae96
vits <vits@norse.digital>
2017-08-15 16:31:05 +0300

    changes

--- 7.1/fpm/scripts/xdebug.sh
+++ 7.1/fpm/scripts/xdebug.sh
@@ -6,6 +6,14 @@ then
     service php7.1-fpm reload
     echo "xdebug disabled"
 else
+    # Retrieve host ip
+    if [ -z "$HOST_IP" ]; then
+        # Allows to set HOST_IP by env variable because could be different from the one which come from ip route command
+        HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
+    fi;
+
+    sed -i "s/xdebug.remote_connect_back=.*/xdebug.remote_host=$HOST_IP/" /etc/php/7.1/mods-available/xdebug.ini
+
     phpenmod xdebug
     service php7.1-fpm reload
     echo "xdebug enabled"
